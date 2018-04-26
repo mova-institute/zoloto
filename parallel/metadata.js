0a636c274e87f8779bdf2c4ba3c2db3c2949ca79
Bohdan Moskalevskyi <msklvsk@icloud.com>
2018-04-26 19:16:27 +0300

    додати приклад метаданих для паралельних корпусів

---
+++ parallel/metadata.js
@@ -0,0 +1,13 @@
+export const documents = [
+  {
+    intertextName: 'Camus_Le_minotaure',
+    originalLang: 'fr',
+    authorsUk: [''],
+    alignments: [
+      {
+        lang: 'uk',
+        authorsUk: ['Дюндик Олена'],
+      }
+    ],
+  },
+]
